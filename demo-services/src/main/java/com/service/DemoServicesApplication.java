664a21bca032cc5d247d24b2c6bac6d62f3176d0
YogeshPatidar6 <patidaryug2018@gmail.com>
2024-04-05 16:43:39 +0530

    test

--- demo-services/src/main/java/com/service/DemoServicesApplication.java
+++ demo-services/src/main/java/com/service/DemoServicesApplication.java
@@ -9,6 +9,8 @@ public class DemoServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServicesApplication.class, args);
 		System.out.println("welcome ...... service start ........");
+System.out.println("this is test");
+
 		
 	}
 
